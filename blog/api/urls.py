fe89687593a8f7736e88a49bcbd5e286013d2a0d
sagarax <haval_sagar@yahoo.com>
2023-08-01 11:56:07 -0600

    Uploaded the files
    From Coursera - Djano Rest Framework course

---
+++ blog/api/urls.py
@@ -0,0 +1,44 @@
+from django.urls import path, include, re_path
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blango API",
+        default_version="v1",
+        description="API for Blango Blog",
+    ),
+    public=True,
+)
+
+urlpatterns = [
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    path("auth/", include("rest_framework.urls")),
+    path("token-auth/", views.obtain_auth_token),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("", include(router.urls)),
+]
+
